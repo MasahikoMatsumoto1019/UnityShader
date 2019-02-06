6e37f21313d407eba249ecb13c08af4df7585114
松本真彦 <match6974@gmail.com>
2019-02-06 15:54:29 +0900

    色々変更

---
+++ SRP/shader/Assets/Script/A.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class A : MonoBehaviour
+{
+    public bool wait = true;
+    Func<IEnumerator> call;
+    public void Open(Func<IEnumerator> call)
+    {
+        wait = true;
+        gameObject.SetActive(true);
+        this.call = null;
+        if (call != null)
+        {
+            this.call = call;
+        }
+    }
+
+    public void OnClickBack()
+    {
+        UpdateRotation.I.StartCoroutine(Boot());
+    }
+
+    public IEnumerator Boot()
+    {
+        Debug.LogWarning("消すよ");
+        gameObject.SetActive(false);
+        if(this.call != null)
+        {
+            Debug.LogWarning("call呼ぶよ");
+            yield return this.call();
+        }
+    }
+
+    public void OnClick()
+    {
+        wait = false;
+    }
+}
--- SRP/shader/Assets/Script/UpdateRotation.cs
+++ SRP/shader/Assets/Script/UpdateRotation.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UpdateRotation : MonoBehaviour {
 
+    public static UpdateRotation I;
+
+    public A dialog;
     public float angle = 0.1f;
 	// Update is called once per frame
 	void Update () {
@@ -12,4 +15,21 @@ public class UpdateRotation : MonoBehaviour {
             tra.RotateAround(tra.position, Vector3.up, angle);
         }
 	}
+
+    // 再帰処理テスト
+
+    private IEnumerator Start()
+    {
+        I = this;
+        yield return a();
+        Debug.Log("StartEnd");
+    }
+
+    IEnumerator a()
+    {
+        dialog.Open(a);
+        while (dialog.wait) yield return null;
+        Debug.Log("a");
+        yield return null;
+    }
 }
